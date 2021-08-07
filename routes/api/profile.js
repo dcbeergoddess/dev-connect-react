607e1b511712e04cc68148714570bf22c9130e7d
Rachel Murray <dcbeergoddess@gmail.com>
2021-08-07 13:56:39 -0400

    get profile by user ID working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -119,4 +119,25 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.find({ user: req.params.user_id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.msg);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).res.send('Server Error');
+  }
+});
+
 module.exports = router;
