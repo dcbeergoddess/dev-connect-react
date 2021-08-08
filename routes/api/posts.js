5702a2f891ec4cc8977c8122cf979c6aecb0e207
Rachel Murray <dcbeergoddess@gmail.com>
2021-08-07 20:08:01 -0400

    delete route working for post by user

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -52,4 +52,53 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // if post does not exist
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    // Check User --> can only delete posts owned by user
+    // post.user is a an object ID and req.user.id is a string so we want them to match
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not Authorized' });
+    }
+    // else (user matches) remove post
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
