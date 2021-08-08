b233c8310ffb276986901243e037bd5a84b9558a
Rachel Murray <dcbeergoddess@gmail.com>
2021-08-08 18:02:13 -0400

    created component for Navbar and Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 
 const App = () => (
   <>
-    <h1>App</h1>
+    <Navbar />
+    <Landing />
   </>
 );
 
