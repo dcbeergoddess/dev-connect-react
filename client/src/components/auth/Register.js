ef03023c3d72e2c52c7df3c19d331b88f8e3622a
Rachel Murray <dcbeergoddess@gmail.com>
2021-08-09 15:54:36 -0400

    destructure props and bring in proptypes

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = props => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     //initial state
     name: '',
@@ -21,7 +22,7 @@ const Register = props => {
   const onSubmit = e => {
     e.preventDefault();
     if (password !== password2) {
-      props.setAlert('Passwords do not match', 'danger');
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log('Success');
     }
@@ -87,6 +88,10 @@ const Register = props => {
       </p>
     </>
   );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
 };
 
 export default connect(null, { setAlert })(Register);
