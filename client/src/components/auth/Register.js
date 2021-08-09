a4188356adae72f4b4c72ea699c14ee82a12edd4
Rachel Murray <dcbeergoddess@gmail.com>
2021-08-09 15:48:32 -0400

    alert in redux dev tools showing up for passwords not matching

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
 
-const Register = () => {
+const Register = props => {
   const [formData, setFormData] = useState({
     //initial state
     name: '',
@@ -19,7 +21,7 @@ const Register = () => {
   const onSubmit = e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      props.setAlert('Passwords do not match', 'danger');
     } else {
       console.log('Success');
     }
@@ -87,4 +89,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default connect(null, { setAlert })(Register);
